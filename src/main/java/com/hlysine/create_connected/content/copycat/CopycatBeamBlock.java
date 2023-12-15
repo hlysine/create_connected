1fad522bda9bc19fca759c303301a693b04e289b
Henry Lin <henry.ys.lin@gmail.com>
2023-12-15 23:27:53 +0800

    Make copycat beams not pathfindable

--- src/main/java/com/hlysine/create_connected/content/copycat/CopycatBeamBlock.java
+++ src/main/java/com/hlysine/create_connected/content/copycat/CopycatBeamBlock.java
@@ -25,6 +25,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -98,6 +99,12 @@ public class CopycatBeamBlock extends WaterloggedCopycatBlock {
         } else {
             return false;
         }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean isPathfindable(@NotNull BlockState pState, @NotNull BlockGetter pLevel, @NotNull BlockPos pPos, @NotNull PathComputationType pType) {
+        return false;
     }
 
     @Override
