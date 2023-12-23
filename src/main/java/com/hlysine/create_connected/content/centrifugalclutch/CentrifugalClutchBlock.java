2f4f17b52eea2a0a7a3c1d5632589e7169338833
Henry Lin <henry.ys.lin@gmail.com>
2023-12-23 13:05:46 +0800

    Fix concurrent modification in freewheel/centrifugal clutch

--- src/main/java/com/hlysine/create_connected/content/centrifugalclutch/CentrifugalClutchBlock.java
+++ src/main/java/com/hlysine/create_connected/content/centrifugalclutch/CentrifugalClutchBlock.java
@@ -2,16 +2,22 @@ package com.hlysine.create_connected.content.centrifugalclutch;
 
 import com.hlysine.create_connected.CCBlockEntityTypes;
 import com.hlysine.create_connected.content.CCBlockStateProperties;
+import com.hlysine.create_connected.content.freewheelclutch.FreewheelClutchBlockEntity;
+import com.simibubi.create.content.kinetics.RotationPropagator;
 import com.simibubi.create.content.kinetics.base.DirectionalKineticBlock;
 import com.simibubi.create.foundation.block.IBE;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import org.jetbrains.annotations.NotNull;
 
 public class CentrifugalClutchBlock extends DirectionalKineticBlock implements IBE<CentrifugalClutchBlockEntity> {
 
@@ -46,5 +52,17 @@ public class CentrifugalClutchBlock extends DirectionalKineticBlock implements I
     @Override
     public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face) {
         return face.getAxis() == state.getValue(FACING).getAxis();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void tick(@NotNull BlockState pState, @NotNull ServerLevel pLevel, @NotNull BlockPos pPos, @NotNull RandomSource pRandom) {
+        BlockEntity be = pLevel.getBlockEntity(pPos);
+        if (!(be instanceof CentrifugalClutchBlockEntity kte))
+            return;
+
+        if (kte.reattachNextTick) {
+            RotationPropagator.handleRemoved(pLevel, pPos, kte);
+        }
     }
 }
--- src/main/java/com/hlysine/create_connected/content/freewheelclutch/FreewheelClutchBlock.java
+++ src/main/java/com/hlysine/create_connected/content/freewheelclutch/FreewheelClutchBlock.java
@@ -2,16 +2,22 @@ package com.hlysine.create_connected.content.freewheelclutch;
 
 import com.hlysine.create_connected.CCBlockEntityTypes;
 import com.hlysine.create_connected.content.CCBlockStateProperties;
+import com.hlysine.create_connected.content.overstressclutch.OverstressClutchBlockEntity;
+import com.simibubi.create.content.kinetics.RotationPropagator;
 import com.simibubi.create.content.kinetics.base.DirectionalKineticBlock;
 import com.simibubi.create.foundation.block.IBE;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import org.jetbrains.annotations.NotNull;
 
 public class FreewheelClutchBlock extends DirectionalKineticBlock implements IBE<FreewheelClutchBlockEntity> {
 
@@ -46,5 +52,17 @@ public class FreewheelClutchBlock extends DirectionalKineticBlock implements IBE
     @Override
     public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face) {
         return face.getAxis() == state.getValue(FACING).getAxis();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void tick(@NotNull BlockState pState, @NotNull ServerLevel pLevel, @NotNull BlockPos pPos, @NotNull RandomSource pRandom) {
+        BlockEntity be = pLevel.getBlockEntity(pPos);
+        if (!(be instanceof FreewheelClutchBlockEntity kte))
+            return;
+
+        if (kte.reattachNextTick) {
+            RotationPropagator.handleRemoved(pLevel, pPos, kte);
+        }
     }
 }
