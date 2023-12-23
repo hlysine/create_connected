8299946869a8730028d2f925520d3d66449dd807
Henry Lin <henry.ys.lin@gmail.com>
2023-12-23 14:06:24 +0800

    Also sync changes if on physical client

--- src/main/java/com/hlysine/create_connected/mixin/SubMenuConfigScreenMixin.java
+++ src/main/java/com/hlysine/create_connected/mixin/SubMenuConfigScreenMixin.java
@@ -4,8 +4,10 @@ import com.hlysine.create_connected.CreateConnected;
 import com.hlysine.create_connected.config.CCConfigs;
 import com.simibubi.create.foundation.config.ui.ConfigScreen;
 import com.simibubi.create.foundation.config.ui.SubMenuConfigScreen;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.util.LogicalSidedProvider;
 import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.util.thread.EffectiveSide;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,7 +22,7 @@ public class SubMenuConfigScreenMixin {
     )
     private void saveChangesAndRefresh(CallbackInfo ci) {
         if (ConfigScreen.modID.equals(CreateConnected.MODID)) {
-            if (EffectiveSide.get().isServer())
+            if (EffectiveSide.get().isServer() || FMLEnvironment.dist == Dist.CLIENT)
                 LogicalSidedProvider.WORKQUEUE.get(LogicalSide.SERVER).submit(() -> CCConfigs.common().syncToAllPlayers());
         }
     }
