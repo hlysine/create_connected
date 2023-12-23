d541a05face0cb3449cb6e6665d6869cf2d7c0fc
Henry Lin <henry.ys.lin@gmail.com>
2023-12-23 12:46:08 +0800

    Sync config when saved via Create's config GUI

---
+++ src/main/java/com/hlysine/create_connected/mixin/SubMenuConfigScreenMixin.java
@@ -0,0 +1,27 @@
+package com.hlysine.create_connected.mixin;
+
+import com.hlysine.create_connected.CreateConnected;
+import com.hlysine.create_connected.config.CCConfigs;
+import com.simibubi.create.foundation.config.ui.ConfigScreen;
+import com.simibubi.create.foundation.config.ui.SubMenuConfigScreen;
+import net.minecraftforge.common.util.LogicalSidedProvider;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.util.thread.EffectiveSide;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = SubMenuConfigScreen.class, remap = false)
+public class SubMenuConfigScreenMixin {
+    @Inject(
+            method = "saveChanges()V",
+            at = @At("TAIL")
+    )
+    private void saveChangesAndRefresh(CallbackInfo ci) {
+        if (ConfigScreen.modID.equals(CreateConnected.MODID)) {
+            if (EffectiveSide.get().isServer())
+                LogicalSidedProvider.WORKQUEUE.get(LogicalSide.SERVER).submit(() -> CCConfigs.common().syncToAllPlayers());
+        }
+    }
+}
