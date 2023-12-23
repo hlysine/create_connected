67069c4fc00859cc71ef20587c8fe5f615b362d3
Henry Lin <henry.ys.lin@gmail.com>
2023-12-23 10:40:45 +0800

    Rework CCCreativeTabs

--- src/main/java/com/hlysine/create_connected/CCCreativeTabs.java
+++ src/main/java/com/hlysine/create_connected/CCCreativeTabs.java
@@ -1,26 +1,14 @@
 package com.hlysine.create_connected;
 
-import com.hlysine.create_connected.config.FeatureToggle;
-import com.simibubi.create.AllCreativeModeTabs;
 import com.tterrag.registrate.util.entry.ItemProviderEntry;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.RegistryObject;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class CCCreativeTabs {
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "create_connected" namespace
-    private static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreateConnected.MODID);
+    public static final CreativeModeTab MAIN  = new MainCreativeModeTab();
 
     public static final List<ItemProviderEntry<?>> ITEMS = List.of(
             CCBlocks.ENCASED_CHAIN_COGWHEEL,
@@ -48,40 +36,24 @@ public class CCCreativeTabs {
             CCBlocks.COPYCAT_VERTICAL_STEP
     );
 
-    public static final RegistryObject<CreativeModeTab> MAIN = CREATIVE_MODE_TABS.register("main", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.create_connected.main"))
-            .withTabsBefore(AllCreativeModeTabs.PALETTES_CREATIVE_TAB.getKey())
-            .icon(CCBlocks.PARALLEL_GEARBOX::asStack)
-            .displayItems(new DisplayItemsGenerator(ITEMS))
-            .build());
+    public static void register() {
+    }
 
-    public static void hideItems(BuildCreativeModeTabContentsEvent event) {
-        if (Objects.equals(event.getTabKey(), MAIN.getKey()) || Objects.equals(event.getTabKey(), CreativeModeTabs.SEARCH)) {
-            Set<Item> hiddenItems = ITEMS.stream()
-                    .filter(x -> !FeatureToggle.isEnabled(x.getId()))
-                    .map(ItemProviderEntry::asItem)
-                    .collect(Collectors.toSet());
-            for (Iterator<Map.Entry<ItemStack, CreativeModeTab.TabVisibility>> iterator = event.getEntries().iterator(); iterator.hasNext(); ) {
-                Map.Entry<ItemStack, CreativeModeTab.TabVisibility> entry = iterator.next();
-                if (hiddenItems.contains(entry.getKey().getItem()))
-                    iterator.remove();
-            }
+    public static class MainCreativeModeTab extends CreativeModeTab {
+
+        public MainCreativeModeTab() {
+            super(CreateConnected.asResource("main").toString());
         }
-    }
 
-    public static void register(IEventBus modEventBus) {
-        CREATIVE_MODE_TABS.register(modEventBus);
-        modEventBus.addListener(CCCreativeTabs::hideItems);
-    }
-
-    private record DisplayItemsGenerator(
-            List<ItemProviderEntry<?>> items) implements CreativeModeTab.DisplayItemsGenerator {
         @Override
-        public void accept(@NotNull CreativeModeTab.ItemDisplayParameters params, @NotNull CreativeModeTab.Output output) {
-            for (ItemProviderEntry<?> item : items) {
-                if (FeatureToggle.isEnabled(item.getId())) {
-                    output.accept(item);
-                }
+        public ItemStack makeIcon() {
+            return CCBlocks.PARALLEL_GEARBOX.asStack();
+        }
+
+        @Override
+        public void fillItemList(NonNullList<ItemStack> pItems) {
+            for (ItemProviderEntry<?> item : ITEMS) {
+                item.get().asItem().fillItemCategory(this, pItems);
             }
         }
     }
--- src/main/java/com/hlysine/create_connected/CreateConnected.java
+++ src/main/java/com/hlysine/create_connected/CreateConnected.java
@@ -50,11 +50,11 @@ public class CreateConnected {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        REGISTRATE.setCreativeTab(CCCreativeTabs.MAIN);
+        REGISTRATE.creativeModeTab(() -> CCCreativeTabs.MAIN);
         CCBlocks.register();
         CCItems.register();
         CCBlockEntityTypes.register();
-        CCCreativeTabs.register(modEventBus);
+        CCCreativeTabs.register();
 
         CCConfigs.register(ModLoadingContext.get());
         CCConfigs.common().register();
