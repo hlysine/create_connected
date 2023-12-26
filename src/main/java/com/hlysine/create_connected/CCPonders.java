ce662ad3321aa7083683be1eddab39cd1f7dd84e
Henry Lin <henry.ys.lin@gmail.com>
2023-12-26 17:03:59 +0800

    Add ponder for Inverted Gearshift

--- src/main/java/com/hlysine/create_connected/CCPonders.java
+++ src/main/java/com/hlysine/create_connected/CCPonders.java
@@ -2,6 +2,7 @@ package com.hlysine.create_connected;
 
 import com.hlysine.create_connected.ponder.ChainCogwheelScenes;
 import com.hlysine.create_connected.ponder.InvertedClutchScenes;
+import com.hlysine.create_connected.ponder.InvertedGearshiftScenes;
 import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
 import com.simibubi.create.foundation.ponder.PonderRegistry;
 import com.simibubi.create.infrastructure.ponder.AllPonderTags;
@@ -22,9 +23,12 @@ public class CCPonders {
                 .addStoryBoard("chain_cogwheel/relay", ChainCogwheelScenes::chainCogwheelAsRelay, AllPonderTags.KINETIC_RELAYS);
         HELPER.forComponents(CCBlocks.INVERTED_CLUTCH)
                 .addStoryBoard("inverted_clutch", InvertedClutchScenes::invertedClutch, AllPonderTags.KINETIC_RELAYS);
+        HELPER.forComponents(CCBlocks.INVERTED_GEARSHIFT)
+                .addStoryBoard("inverted_gearshift", InvertedGearshiftScenes::invertedGearshift, AllPonderTags.KINETIC_RELAYS);
 
         PonderRegistry.TAGS.forTag(AllPonderTags.KINETIC_RELAYS)
                 .add(CCBlocks.ENCASED_CHAIN_COGWHEEL)
-                .add(CCBlocks.INVERTED_CLUTCH);
+                .add(CCBlocks.INVERTED_CLUTCH)
+                .add(CCBlocks.INVERTED_GEARSHIFT);
     }
 }
