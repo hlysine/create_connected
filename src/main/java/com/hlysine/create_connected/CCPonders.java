63e40959ca57850b7ffdbe1923668830e13245f8
Henry Lin <henry.ys.lin@gmail.com>
2023-12-26 16:53:01 +0800

    Remove tooltips for components with ponder

--- src/main/java/com/hlysine/create_connected/CCPonders.java
+++ src/main/java/com/hlysine/create_connected/CCPonders.java
@@ -6,6 +6,13 @@ import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
 import com.simibubi.create.foundation.ponder.PonderRegistry;
 import com.simibubi.create.infrastructure.ponder.AllPonderTags;
 
+/**
+ * Steps to add ponders for an existing component:
+ * 1. Create a schematic .nbt file in resources/ponder
+ * 2. Create a [Component]Scenes file in src/ponder with scene instructions
+ * 3. Register the file and the scenes here
+ * 4. Remove the placeholder tooltip in resources/lang/default/tooltips.json
+ */
 public class CCPonders {
 
     static final PonderRegistrationHelper HELPER = new PonderRegistrationHelper(CreateConnected.MODID);
