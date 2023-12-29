3c6d063831d8866b128950e7ecf1b50eedebdde5
Henry Lin <henry.ys.lin@gmail.com>
2023-12-29 09:30:58 +0800

    Add ponder for sequenced pulse generator

--- src/main/java/com/hlysine/create_connected/CCPonders.java
+++ src/main/java/com/hlysine/create_connected/CCPonders.java
@@ -1,9 +1,6 @@
 package com.hlysine.create_connected;
 
-import com.hlysine.create_connected.ponder.ChainCogwheelScenes;
-import com.hlysine.create_connected.ponder.InvertedClutchScenes;
-import com.hlysine.create_connected.ponder.InvertedGearshiftScenes;
-import com.hlysine.create_connected.ponder.ParallelGearboxScenes;
+import com.hlysine.create_connected.ponder.*;
 import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
 import com.simibubi.create.foundation.ponder.PonderRegistry;
 import com.simibubi.create.infrastructure.ponder.AllPonderTags;
@@ -21,18 +18,22 @@ public class CCPonders {
 
     public static void register() {
         HELPER.forComponents(CCBlocks.ENCASED_CHAIN_COGWHEEL)
-                .addStoryBoard("chain_cogwheel/relay", ChainCogwheelScenes::chainCogwheelAsRelay, AllPonderTags.KINETIC_RELAYS);
+                .addStoryBoard("chain_cogwheel", ChainCogwheelScenes::chainCogwheelAsRelay, AllPonderTags.KINETIC_RELAYS);
         HELPER.forComponents(CCBlocks.INVERTED_CLUTCH)
                 .addStoryBoard("inverted_clutch", InvertedClutchScenes::invertedClutch, AllPonderTags.KINETIC_RELAYS);
         HELPER.forComponents(CCBlocks.INVERTED_GEARSHIFT)
                 .addStoryBoard("inverted_gearshift", InvertedGearshiftScenes::invertedGearshift, AllPonderTags.KINETIC_RELAYS);
         HELPER.forComponents(CCBlocks.PARALLEL_GEARBOX, CCItems.VERTICAL_PARALLEL_GEARBOX)
                 .addStoryBoard("parallel_gearbox", ParallelGearboxScenes::parallelGearbox, AllPonderTags.KINETIC_RELAYS);
+        HELPER.forComponents(CCBlocks.SEQUENCED_PULSE_GENERATOR)
+                .addStoryBoard("sequenced_pulse_generator", SequencedPulseGeneratorScenes::pulseGenerator, AllPonderTags.REDSTONE);
 
         PonderRegistry.TAGS.forTag(AllPonderTags.KINETIC_RELAYS)
                 .add(CCBlocks.ENCASED_CHAIN_COGWHEEL)
                 .add(CCBlocks.INVERTED_CLUTCH)
                 .add(CCBlocks.INVERTED_GEARSHIFT)
                 .add(CCBlocks.PARALLEL_GEARBOX);
+        PonderRegistry.TAGS.forTag(AllPonderTags.REDSTONE)
+                .add(CCBlocks.SEQUENCED_PULSE_GENERATOR);
     }
 }
