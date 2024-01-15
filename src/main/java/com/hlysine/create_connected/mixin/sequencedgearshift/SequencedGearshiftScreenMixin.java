3460f1e06accd9ad77e9825a55c5f03eeb66830c
Henry Lin <henry.ys.lin@gmail.com>
2024-01-15 20:46:06 +0800

    Remove debug annotation from SequencedGearshiftScreenMixin

--- src/main/java/com/hlysine/create_connected/mixin/sequencedgearshift/SequencedGearshiftScreenMixin.java
+++ src/main/java/com/hlysine/create_connected/mixin/sequencedgearshift/SequencedGearshiftScreenMixin.java
@@ -4,7 +4,6 @@ import com.hlysine.create_connected.CCSequencerInstructions;
 import com.simibubi.create.content.kinetics.transmission.sequencer.Instruction;
 import com.simibubi.create.content.kinetics.transmission.sequencer.SequencedGearshiftScreen;
 import com.simibubi.create.foundation.gui.widget.ScrollInput;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,7 +13,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.Vector;
 
 @Mixin(value = SequencedGearshiftScreen.class, remap = false)
-@Debug(export = true)
 public class SequencedGearshiftScreenMixin {
     @Shadow
     private Vector<Instruction> instructions;
