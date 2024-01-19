74d1b2938e9f6cc4f94c16d952c478f36768f80f
Henry Lin <henry.ys.lin@gmail.com>
2024-01-19 16:00:47 +0800

    Make "loop" a terminal instruction

---
+++ src/main/java/com/hlysine/create_connected/mixin/sequencedgearshift/AbstractSimiScreenAccessor.java
@@ -0,0 +1,14 @@
+package com.hlysine.create_connected.mixin.sequencedgearshift;
+
+import com.simibubi.create.foundation.gui.AbstractSimiScreen;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.Collection;
+
+@Mixin(AbstractSimiScreen.class)
+public interface AbstractSimiScreenAccessor {
+    @Invoker
+    void callRemoveWidgets(Collection<? extends GuiEventListener> widgets);
+}
