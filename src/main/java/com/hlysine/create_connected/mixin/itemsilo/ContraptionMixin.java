f18aeb7219ca79babaad7bf8d1a4636370d20f76
Henry Lin <henry.ys.lin@gmail.com>
2024-09-17 12:22:05 +0800

    Fix ticking BE crash in item silo

--- src/main/java/com/hlysine/create_connected/mixin/itemsilo/ContraptionMixin.java
+++ src/main/java/com/hlysine/create_connected/mixin/itemsilo/ContraptionMixin.java
@@ -1,41 +1,58 @@
 package com.hlysine.create_connected.mixin.itemsilo;
 
-import com.hlysine.create_connected.content.itemsilo.ItemSiloBlockEntity;
+import com.google.common.collect.Multimap;
+import com.hlysine.create_connected.content.itemsilo.ItemSiloBlock;
 import com.simibubi.create.content.contraptions.Contraption;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Data fixer for contraptions with corrupted item silo NBT
+ */
 @Mixin(value = Contraption.class, remap = false)
-public abstract class ContraptionMixin {
+public class ContraptionMixin {
     @Shadow
-    protected abstract BlockPos toLocalPos(BlockPos globalPos);
+    protected Map<BlockPos, StructureTemplate.StructureBlockInfo> blocks;
+
+    @Shadow
+    protected Multimap<BlockPos, StructureTemplate.StructureBlockInfo> capturedMultiblocks;
 
     @Inject(
-            at = @At("HEAD"),
-            method = "getBlockEntityNBT(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/nbt/CompoundTag;",
-            cancellable = true
+            at = @At("RETURN"),
+            method = "readNBT"
     )
-    private void getItemSiloNBT(Level world, BlockPos pos, CallbackInfoReturnable<CompoundTag> cir) {
-        BlockEntity blockEntity = world.getBlockEntity(pos);
-        if (blockEntity instanceof ItemSiloBlockEntity) {
-            CompoundTag nbt = blockEntity.saveWithFullMetadata();
-            nbt.remove("x");
-            nbt.remove("y");
-            nbt.remove("z");
-
-            if (nbt.contains("Controller"))
-                nbt.put("Controller",
-                        NbtUtils.writeBlockPos(toLocalPos(NbtUtils.readBlockPos(nbt.getCompound("Controller")))));
-
-            cir.setReturnValue(nbt);
+    private void fixNBT(Level world, CompoundTag nbt, boolean spawnData, CallbackInfo ci) {
+        for (Map.Entry<BlockPos, StructureTemplate.StructureBlockInfo> entry : blocks.entrySet()) {
+            if (!(entry.getValue().state().getBlock() instanceof ItemSiloBlock))
+                return;
+            if (entry.getValue().nbt().contains("Length") && !NbtUtils.readBlockPos(entry.getValue().nbt().getCompound("Controller")).equals(entry.getKey())) {
+                entry.getValue().nbt().put("Controller", NbtUtils.writeBlockPos(entry.getKey()));
+            }
+        }
+        for (Iterator<BlockPos> iterator = capturedMultiblocks.keys().iterator(); iterator.hasNext(); ) {
+            BlockPos pos = iterator.next();
+            if (capturedMultiblocks.get(pos).stream().noneMatch(info -> info.state().getBlock() instanceof ItemSiloBlock))
+                continue;
+            if (!blocks.containsKey(pos)) {
+                iterator.remove();
+                capturedMultiblocks.put(pos, blocks.get(pos));
+                continue;
+            }
+            if (!blocks.get(pos).nbt().contains("Length")) {
+                iterator.remove();
+                capturedMultiblocks.put(pos, blocks.get(pos));
+            }
         }
     }
 }
