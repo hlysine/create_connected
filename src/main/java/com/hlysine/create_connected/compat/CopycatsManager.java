08248610dedb906bb9c9daa519ced48abe4fcb12
Henry Lin <henry.ys.lin@gmail.com>
2025-05-18 17:37:28 +0800

    Update copycats dependency

--- src/main/java/com/hlysine/create_connected/compat/CopycatsManager.java
+++ src/main/java/com/hlysine/create_connected/compat/CopycatsManager.java
@@ -16,7 +16,6 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
-import net.neoforged.fml.LogicalSide;
 import net.neoforged.neoforge.event.tick.LevelTickEvent;
 
 import java.util.*;
