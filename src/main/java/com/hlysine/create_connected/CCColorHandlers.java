fcdced5ee276d80db9087751f9dc7d9f132c9706
Henry Lin <henry.ys.lin@gmail.com>
2025-05-19 18:57:47 +0800

    Fix water color in fan splashing catalyst

--- src/main/java/com/hlysine/create_connected/CCColorHandlers.java
+++ src/main/java/com/hlysine/create_connected/CCColorHandlers.java
@@ -3,6 +3,9 @@ package com.hlysine.create_connected;
 import net.minecraft.client.color.block.BlockColor;
 import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.world.level.material.Fluids;
+import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
+import net.neoforged.neoforge.fluids.FluidStack;
 
 public class CCColorHandlers {
 
@@ -11,7 +14,9 @@ public class CCColorHandlers {
                 level != null && pos != null ? BiomeColors.getAverageWaterColor(level, pos) : -1;
     }
 
+    private static final FluidStack waterStack = new FluidStack(Fluids.WATER, 1);
+
     public static ItemColor waterItemTint() {
-        return (stack, tintIndex) -> 4159204; // default water color
+        return (stack, tintIndex) -> IClientFluidTypeExtensions.of(Fluids.WATER).getTintColor(waterStack); // default water color
     }
 }
