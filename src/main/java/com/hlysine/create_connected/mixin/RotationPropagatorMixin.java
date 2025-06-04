165917737a3a40776f19f28f3293317513357549
Henry Lin <henry.ys.lin@gmail.com>
2025-06-04 22:31:45 +0800

    Fix incompatibility with C:BC due to immutable list of neighbor locations

--- src/main/java/com/hlysine/create_connected/mixin/RotationPropagatorMixin.java
+++ src/main/java/com/hlysine/create_connected/mixin/RotationPropagatorMixin.java
@@ -3,19 +3,15 @@ package com.hlysine.create_connected.mixin;
 import com.hlysine.create_connected.content.IConnectionForwardingBlock;
 import com.hlysine.create_connected.content.ISplitShaftBlockEntity;
 import com.simibubi.create.content.kinetics.RotationPropagator;
-import com.simibubi.create.content.kinetics.base.DirectionalShaftHalvesBlockEntity;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
-import com.simibubi.create.content.kinetics.gearbox.GearboxBlockEntity;
-import com.simibubi.create.content.kinetics.transmission.SplitShaftBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(RotationPropagator.class)
@@ -41,7 +37,7 @@ public abstract class RotationPropagatorMixin {
             cancellable = true
     )
     private static void forwardConnection(KineticBlockEntity be, CallbackInfoReturnable<List<BlockPos>> cir) {
-        List<BlockPos> positions = cir.getReturnValue();
+        List<BlockPos> positions = new ArrayList<>(cir.getReturnValue());
         for (int i = 0; i < positions.size(); i++) {
             BlockPos sourcePos = be.getBlockPos();
             BlockPos neighborPos = positions.get(i);
