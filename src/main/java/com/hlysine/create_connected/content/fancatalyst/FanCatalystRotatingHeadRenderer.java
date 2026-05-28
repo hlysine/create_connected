d095622c921b378c19089b67be2230dfe0a64cb4
JustAGuy444 <281965879+JustAGuy4447@users.noreply.github.com>
2026-05-29 00:12:46 +0200

    Added rotating Creeper head to the Fan Exploding Catalyst + The rotating head renderer for Fan Catalysts is less hard-coded

---
+++ src/main/java/com/hlysine/create_connected/content/fancatalyst/FanCatalystRotatingHeadRenderer.java
@@ -0,0 +1,35 @@
+package com.hlysine.create_connected.content.fancatalyst;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.simibubi.create.foundation.blockEntity.renderer.SafeBlockEntityRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.resources.ResourceLocation;
+
+public class FanCatalystRotatingHeadRenderer extends SafeBlockEntityRenderer<FanCatalystRotatingHeadBlockEntity> {
+    private final SkullTypes skullType;
+    private float animationTick;
+
+    public FanCatalystRotatingHeadRenderer(SkullTypes skullType) {
+        this.skullType = skullType;
+    }
+
+    public RenderType getRenderType() {
+        ResourceLocation resourcelocation = SkullBlockRenderer.SKIN_BY_TYPE.get(skullType.getTexture());
+        return RenderType.entityCutoutNoCullZOffset(resourcelocation);
+    }
+
+    @Override
+    protected void renderSafe(FanCatalystRotatingHeadBlockEntity be, float partialTicks, PoseStack ms, MultiBufferSource bufferSource, int light, int overlay) {
+        animationTick += partialTicks;
+        animationTick %= 360;
+        ms.pushPose();
+        skullType.translate(ms);
+        skullType.scale(ms);
+        skullType.getModel().setupAnim(0, animationTick, 0.0F);
+        skullType.getModel().renderToBuffer(ms, bufferSource.getBuffer(getRenderType()), light, OverlayTexture.NO_OVERLAY);
+        ms.popPose();
+    }
+}
