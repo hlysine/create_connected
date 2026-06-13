e859f8981cb019e34d6021a678dee8bb7bf5d1ed
Henry Lin <henry.ys.lin@gmail.com>
2026-06-13 12:09:37 +0800

    Fire neoforge events when updating JEI item list for modpack compatibility

--- src/main/java/com/hlysine/create_connected/compat/CreateConnectedJEI.java
+++ src/main/java/com/hlysine/create_connected/compat/CreateConnectedJEI.java
@@ -11,6 +11,7 @@ import mezz.jei.api.runtime.IIngredientManager;
 import mezz.jei.api.runtime.IJeiRuntime;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.NeoForge;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.stream.Collectors;
@@ -34,6 +35,7 @@ public class CreateConnectedJEI implements IModPlugin {
 
     public static void refreshItemList() {
         if (MANAGER != null && Minecraft.getInstance().level != null) {
+            NeoForge.EVENT_BUS.post(new FeatureRefreshEvent.Pre(ID, MANAGER));
             MANAGER.removeIngredientsAtRuntime(
                     VanillaTypes.ITEM_STACK,
                     CCCreativeTabs.ITEMS.stream()
@@ -47,6 +49,7 @@ public class CreateConnectedJEI implements IModPlugin {
                             .map(ItemProviderEntry::asStack)
                             .collect(Collectors.toList())
             );
+            NeoForge.EVENT_BUS.post(new FeatureRefreshEvent.Post(ID, MANAGER));
         }
     }
 }
---
+++ src/main/java/com/hlysine/create_connected/compat/FeatureRefreshEvent.java
@@ -0,0 +1,47 @@
+package com.hlysine.create_connected.compat;
+
+import mezz.jei.api.runtime.IIngredientManager;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.Event;
+
+/*
+ * Base class of the two feature refresh events.
+ *
+ * @see FeatureRefreshEvent.Pre
+ * @see FeatureRefreshEvent.Post
+ */
+public class FeatureRefreshEvent extends Event {
+    private final ResourceLocation jeiPluginId;
+    private final IIngredientManager ingredientManager;
+
+    protected FeatureRefreshEvent(ResourceLocation jeiPluginId, IIngredientManager ingredientManager) {
+        this.jeiPluginId = jeiPluginId;
+        this.ingredientManager = ingredientManager;
+    }
+
+    public ResourceLocation getJeiPluginId() {
+        return jeiPluginId;
+    }
+
+    public IIngredientManager getIngredientManager() {
+        return ingredientManager;
+    }
+
+    /**
+     * Fired before Create: Connected updates the JEI item list according to enabled features.
+     */
+    public static class Pre extends FeatureRefreshEvent {
+        public Pre(ResourceLocation jeiPluginId, IIngredientManager ingredientManager) {
+            super(jeiPluginId, ingredientManager);
+        }
+    }
+
+    /**
+     * Fired after Create: Connected updates the JEI item list according to enabled features.
+     */
+    public static class Post extends FeatureRefreshEvent {
+        public Post(ResourceLocation jeiPluginId, IIngredientManager ingredientManager) {
+            super(jeiPluginId, ingredientManager);
+        }
+    }
+}
